b2ab12ebe06bd94c22854b12653687ed2061b832
sassafras13 <benjaminsonemma9@gmail.com>
2021-12-01 12:11:43 -0500

    add more bash scripts and update animation script to save gif

--- src/learning_to_simulate/render_rollout.py
+++ src/learning_to_simulate/render_rollout.py
@@ -96,6 +96,7 @@ def main(unused_argv):
   unused_animation = animation.FuncAnimation(
       fig, update,
       frames=np.arange(0, num_steps, FLAGS.step_stride), interval=10)
+  unused_animation.save("animation.gif", writer="imagemagick", fps=10)
   plt.show(block=FLAGS.block_on_show)
 
 
---
+++ src/plot_results.sh
@@ -0,0 +1,1 @@
+python -m learning_to_simulate.render_rollout --rollout_path=/home/emma/gnn_dna/rollouts/rollout_test_0.pkl --step_stride=1
