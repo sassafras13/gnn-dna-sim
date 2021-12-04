c06a0e100bf3bb2ebb6340526528e76e6e6ac32d
sassafras13 <benjaminsonemma9@gmail.com>
2021-12-04 17:00:44 -0500

    add plotting function for val, train loss curves

--- src/learning_to_simulate/plot_loss_curves.py
+++ src/learning_to_simulate/plot_loss_curves.py
@@ -2,9 +2,23 @@
 
 # import data using pandas
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-filename = "/home/emma/Documents/Classes/10-708/valloss.csv"
-data = np.loadtxt(filename, dtype=float)
-print("data")
+trainfilename = "/home/emma/Documents/Classes/10-708/trainloss.csv"
+df1 = pd.read_csv(trainfilename)
+print(df1.head())
+df1 = df1.rename(columns={"Value":"Train Loss"})
+
+valfilename = "/home/emma/Documents/Classes/10-708/valloss.csv"
+df = pd.read_csv(valfilename)
+df = df.rename(columns={"Value":"Validation Loss"})
+print(df.head())
+
+val_loss = df["Validation Loss"]
+df1 = df1.join(val_loss)
+print(df1.head())
 
 # plot curves
+df1.plot(x="Step", y=["Train Loss", "Validation Loss"])
+plt.show()
