corrected time index calculation in dataset getitem function

src/gnn/data.py
@@ -33,7 +33,8 @@ class DatasetGraph(Dataset):
         the time step interval between steps in the trajectory
     n_timesteps : int
         the total number of timesteps in each trajectory in the dataset
-
+    time_index : int
+        the current timestep we are considering
 
     Methods: 
     --------
@@ -81,8 +82,8 @@ class DatasetGraph(Dataset):
         Loads and returns a sample from the dataset at the given index. Note that the index must be a scalar value for the DataLoader to work, so we can convert the scalar value to refer to the i-th trajectory and j-th time step as:
         M = number of trajectories
         N = number of time steps
-        i = traj_idx
-        j = time_idx
+        i = traj_idx (from 0 to M-1)
+        j = time_idx (from 0 to N-1)
 
         idx = (i * N) + (j * 1) 
 
@@ -90,6 +91,12 @@ class DatasetGraph(Dataset):
 
         j = idx % N 
         i = (idx - j) / N 
+
+        We convert j to simulation time by:
+        
+        time_idx = (j + 1) * dt
+        
+        This is what we will need to use to lookup data for a specific time point in the training data. 
 
         Once we have the trajectory index (i) and time step index (j) we can generate the necessary datapoint which is (X, E, edge_attr, edge_index, y), i.e. (node attribute matrix, edge attribute matrix, reshaped edge attribute matrix, reshaped edge index matrix, output dynamics).
 
@@ -107,15 +114,15 @@ class DatasetGraph(Dataset):
         (X, E, edge_attr, edge_index, y) : (np.array, np.array, np.array, np.array, np.array)
             The set of data the model needs to predict the next yhat value        
         """
-        time_idx = int(index % self.n_timesteps)
-        graph_idx = int((index - time_idx) / self.n_timesteps)
+        j = int(index % self.n_timesteps) 
+        self.time_idx = (j + 1) * self.dt
+        graph_idx = int((index - j) / self.n_timesteps)
 
         self.traj_file = self.traj_list[graph_idx]
-        # print("Trajectory file = ", self.traj_file)
-        X, E = makeGraphfromTraj(self.top_file, self.traj_file, self.n_nodes, self.n_features)
+        _, E = makeGraphfromTraj(self.top_file, self.traj_file, self.n_nodes, self.n_features)
         edge_attr, edge_index = prepareEForModel(E)
-        # X = buildX(self.traj_file, time_idx, self.n_nodes, self.n_features)
-        y = getGroundTruthY(self.traj_file, time_idx, self.dt, self.n_nodes, self.n_features)
+        X = buildX(self.traj_file, self.time_idx, self.n_nodes, self.n_features)
+        y = getGroundTruthY(self.traj_file, self.time_idx, self.dt, self.n_nodes, self.n_features)
         return (X, E, edge_attr, edge_index, y)
     
     def __len__(self) -> int: 

src/gnn/test.py
@@ -42,14 +42,13 @@ class TestGetItem(TestDataset):
     
     def test_getitem1(self):
         """
-        Check that X data is correct.
+        Check that X data is correct for first time step.
         """
         self.item = self.myDataset.__getitem__(0)
         X_row1 = torch.tensor([[ 0.0000e+00,  1.03521e+01,  1.62153e+01,  1.50945e+01,  8.7061e-01,
          -4.0965e-01,  2.7245e-01,  3.1363e-01,  3.5480e-02, -9.4888e-01,
          -2.1492e-02, -2.1492e-01, -1.2970e-01, -8.5849e-02,  2.3618e-01,
           2.1333e-01]]) # first row for first time step in traj7.dat
-        # print(X_row1)
         self.assertAlmostEqual(float(torch.sum(self.item[0][0])), float(torch.sum(X_row1)), places=3)
         self.assertEqual(self.item[0].shape, torch.Size([40,16]))
 
@@ -58,8 +57,6 @@ class TestGetItem(TestDataset):
         Check that E data is correct.
         """
         self.item = self.myDataset.__getitem__(0)
-        # print(item[1])
-        # print(int(torch.sum(item[1][0,:])))
         self.assertEqual(int(torch.sum(self.item[1][0,:])), 1)
         self.assertEqual(int(torch.sum(self.item[1][1,:])), 2)
         self.assertEqual(self.item[1].shape, torch.Size([40,40]))
@@ -69,7 +66,6 @@ class TestGetItem(TestDataset):
         Check that edge attr is correct.
         """
         self.item = self.myDataset.__getitem__(0)
-        # print(self.item[2])
         self.assertEqual(self.item[2].shape, torch.Size([76,1]))
 
     def test_getitem4(self):
@@ -77,10 +73,28 @@ class TestGetItem(TestDataset):
         Check that edge_index is correct.
         """
         self.item = self.myDataset.__getitem__(0)
-        # print(self.item[3])
-        # print(torch.sum(self.item[3][0]))
         self.assertEqual(self.item[3].shape, torch.Size([2, 76]))
         self.assertEqual(int(torch.sum(self.item[3][0])), 1482)
+
+    def test_getitem5(self):
+        """
+        Check that the function computes the correct time index.
+        """
+        self.item = self.myDataset.__getitem__(0)
+        self.assertEqual(self.myDataset.time_idx, 100)
+
+        self.item2 = self.myDataset.__getitem__(3)
+        self.assertEqual(self.myDataset.time_idx, 400)
+
+    def test_getitem6(self):
+        """
+        Check that the function pulls the correct X data for a later time point.
+        """
+        self.item = self.myDataset.__getitem__(3)
+        X_row1 = torch.tensor([[ 0.0000, 10.5120, 16.2222, 15.0403,  0.8090, -0.4704,  0.3526,  0.3778,
+        -0.0436, -0.9249,  0.5860,  0.2361, -0.1389,  0.2467, -0.1906,  0.2378]]) # fourth row for first time step in traj7.dat
+        self.assertAlmostEqual(float(torch.sum(self.item[0][0])), float(torch.sum(X_row1)), places=3)
+        self.assertEqual(self.item[0].shape, torch.Size([40,16]))
 
 class TestLen(TestDataset):
     
@@ -91,16 +105,16 @@ class TestLen(TestDataset):
         self.len = self.myDataset.__len__()
         self.assertEqual(self.len, 2)
 
-class TestUtils(unittest.TestCase)
+class TestUtils(unittest.TestCase):
     def setUp(self):
         self.top_file = "./test_data/top.top"
         self.traj_file = "./test_data/trajectory_sim_traj3.dat"
         self.n_nodes = 40
         self.n_features = 16
 
-class TestMakeGraphfromTraj(TestUtils):
+# class TestMakeGraphfromTraj(TestUtils):
 
-    def test
+#     def test
 
 if __name__ == '__main__':
     unittest.main()