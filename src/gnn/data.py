Add noise to X in dataset class, and update calculation of doUpdate and getGroundTruthY to use correct time interval

src/gnn/data.py
@@ -58,7 +58,9 @@ class DatasetGraph(Dataset):
                  n_features: int,
                  dt: int, 
                  n_timesteps: int,
-                 k : int = 3):
+                 k : int = 3, 
+                 gnd_time_interval : float = 0.005, 
+                 noise_std : float = 0.003):
         """
         Initializes the class attributes. 
 
@@ -76,6 +78,8 @@ class DatasetGraph(Dataset):
             the total number of timesteps in each trajectory in the dataset
         k : int
             number of nearest neighbors used to compute adjacency matrix
+        gnd_time_interval : float
+            Time represented by one time step in the ground truth data
         """
         self.top_file = dir + "top.top" 
         self.traj_list = glob.glob(dir + "trajectory_sim_traj*.dat")
@@ -84,6 +88,8 @@ class DatasetGraph(Dataset):
         self.dt = dt
         self.n_timesteps = n_timesteps
         self.k = k
+        self.gnd_time_interval = gnd_time_interval
+        self.noise_std = noise_std
 
         # build the edge information for the graph because this will not change (for now) from trajectory file to trajectory file
         _, self.E_backbone = makeGraphfromTraj(self.top_file, self.traj_list[0], self.n_nodes, self.n_features)
@@ -141,23 +147,21 @@ class DatasetGraph(Dataset):
         X = self.full_X[j]
         X[:,0] = self.tmp_X[:,0] # adds information about the nucleotide type
 
+        # add noise to velocity, angular velocity of size [1, 6]
+        noise = torch.empty(self.n_nodes, 6).normal_(mean=0,std=self.noise_std)
+        X[:,-6:] = X[:,-6:] + noise
+
         # build up the adjacency matrix, E, for this time step
         # compute E_neighbors by providing X[:,1:3] to knn_graph and asking for k nearest neighbors
-        # E_knn = getKNN(X[:,1:4], self.k)
         output = knn_graph(X[:,1:4], self.k, flow="target_to_source")
         row = output[0,:]
         col = output[1,:]
         data = torch.ones_like(row)
         coo = coo_matrix((data, (row, col)), shape=(X.shape[0], X.shape[0]))
         E_knn = torch.from_numpy(coo.todense())
-        # print("output = ", output)
         
         # combines the backbone edges and knn edges
-        # print("E_knn = ", E_knn)
-        # print("backbone = ", self.E_backbone)
         self.E = E_knn + self.E_backbone
-        # print("E = ", self.E)
-        # plotGraph(X, self.E)
 
         # convert the output to a coo-matrix
         edges_coo = coo_matrix(self.E)
@@ -169,7 +173,7 @@ class DatasetGraph(Dataset):
         self.edge_index = torch.from_numpy(edge_index)
         self.edge_attr = torch.from_numpy(edge_attr.T)
 
-        y = getGroundTruthY(self.traj_file, j, self.full_X, self.dt, self.n_nodes, self.n_features)
+        y = getGroundTruthY(self.traj_file, j, self.full_X, self.dt, self.n_nodes, self.n_features, self.gnd_time_interval)
         return (X, self.E, self.edge_attr, self.edge_index, y)
     
     def __len__(self) -> int: 

src/gnn/main.py
@@ -40,6 +40,8 @@ def parse_arguments():
     parser.add_argument("--architecture", type=str, default="gnn", help="Can select 'gnn' or 'mlp'.")
     parser.add_argument("--seed", type=int, default=None, help="Random seed")
     parser.add_argument("--k", type=int, default=3, help="Number of neighbors to include in adjacency matrix.")
+    parser.add_argument("--noise_std", type=float, default=0.003, help="Std deviation of noise to add to training data.")
+    parser.add_argument("--gnd_time_interval", type=float, default=0.005, help="Time interval for one time step in original simulation tool.")
     args, unknown = parser.parse_known_args()
     return args
 
@@ -70,6 +72,8 @@ def main(args):
     seed = args.seed
     architecture = args.architecture
     k = args.k
+    noise_std = args.noise_std
+    gnd_time_interval = args.gnd_time_interval
 
     # start a new wandb run to track this script
     run = wandb.init(
@@ -94,7 +98,9 @@ def main(args):
             "n_train" : n_train,
             "n_val" : n_val,
             "architecture" : architecture,
-            "k" : k
+            "k" : k,
+            "noise_std" : noise_std,
+            "gnd_time_interval" : gnd_time_interval
             }
     )
 
@@ -109,9 +115,9 @@ def main(args):
     print("Using {} device".format(device))
 
     # --- init dataset and dataloader classes ---
-    train_dataset = DatasetGraph(train_dir, n_nodes, n_features, dt, n_timesteps, k)
+    train_dataset = DatasetGraph(train_dir, n_nodes, n_features, dt, n_timesteps, k, gnd_time_interval, noise_std)
     train_dataloader = DataloaderGraph(train_dataset, n_timesteps, shuffle=True)
-    val_dataset = DatasetGraph(val_dir, n_nodes, n_features, dt, n_timesteps, k)
+    val_dataset = DatasetGraph(val_dir, n_nodes, n_features, dt, n_timesteps, k, gnd_time_interval, noise_std=0)
     val_dataloader = DataloaderGraph(val_dataset, n_timesteps, shuffle=False)
 
     # --- plot the graph ---
@@ -121,9 +127,9 @@ def main(args):
 
     # --- model ---
     if architecture == "mlp":
-        model = MlpModel(n_features, n_latent, Y_features)
+        model = MlpModel(n_features, n_latent, Y_features, gnd_time_interval)
     else:
-        model = GNN(n_nodes, n_edges, n_features, n_latent, Y_features) # KEEP 
+        model = GNN(n_nodes, n_edges, n_features, n_latent, Y_features, gnd_time_interval) # KEEP 
 
     # --- loss function ---
     loss_fn = nn.MSELoss() # this is used for training the model

src/gnn/model.py
@@ -30,7 +30,7 @@ class MlpModel(nn.Module):
         Computes a trajectory for rollout_steps number of steps, starting with the structure described by top_file and traj_file.     
     """
 
-    def __init__(self, n_features, n_latent, Y_features):
+    def __init__(self, n_features, n_latent, Y_features, gnd_time_interval):
         """
         Initialize the MLP model.
 
@@ -42,11 +42,15 @@ class MlpModel(nn.Module):
             the size of the latent space to be used in the hidden layers of the model
         Y_features : int
             the number of unique features in the output, y, which is the acceleration information for each node
+        gnd_time_interval : int
+            Time represented by one time step in the ground truth data
 
         Returns:
         --------
 
         """
+        self.gnd_time_interval = gnd_time_interval 
+
         super(MlpModel, self).__init__()
 
         self.mlp = nn.Sequential(
@@ -122,7 +126,7 @@ class MlpModel(nn.Module):
             for k in tqdm(range(rollout_steps)):
                 t += dt
                 y_h = self(X) 
-                X_next = doUpdate(X, y_h, dt)
+                X_next = doUpdate(X, y_h, dt, self.gnd_time_interval)
 
                 # save the X_next to file
                 with open(rollout_traj_file, "a") as f:
@@ -714,7 +718,7 @@ class GNN(nn.Module):
     rollout(rollout_steps, rollout_traj_file, t, top_file, traj_file, dt, N)
         Generates a novel rollout trajectory used to qualitatively verify how the model performs. 
     """
-    def __init__(self, n_nodes, n_edges, n_features, n_latent, Y_features): 
+    def __init__(self, n_nodes, n_edges, n_features, n_latent, Y_features, gnd_time_interval): 
         """
         Initializes an instance of the GNN class.
 
@@ -730,10 +734,14 @@ class GNN(nn.Module):
             The size of the latent space
         Y_features : int
             the number of unique features in the output, y, which is the acceleration information for each node
-
+        gnd_time_interval : int
+            Time represented by one time step in the ground truth data
+        
         Returns:
         --------
         """
+        self.gnd_time_interval = gnd_time_interval
+
         super(GNN, self).__init__()
         self.encoder_model = Encoder(n_edges, n_features, n_latent)
         self.processor_model = Processor(n_nodes, n_edges, n_latent)
@@ -776,7 +784,7 @@ class GNN(nn.Module):
         y_h = self.decoder_model(X_m)
 
         # --- update function ---
-        X_next = doUpdate(X, y_h, dt)
+        X_next = doUpdate(X, y_h, dt, self.gnd_time_interval)
 
         # --- loss function ---
         # the loss function needs to compare the predicted acceleration with the target acceleration for a randomly selected set of nucleotides

src/gnn/test.py
@@ -5,6 +5,9 @@ import torch
 import time
 from tqdm import tqdm
 
+torch.manual_seed(13)
+
+
 class TestDataset(unittest.TestCase):
     def setUp(self):
         self.dir = "./test_data/"
@@ -37,10 +40,13 @@ class TestGetItem(TestDataset):
         Check that X data is correct for first time step.
         """
         self.item = self.myDataset.__getitem__(0)
-        X_row1 = torch.tensor([[ 0.0000e+00,  1.03521e+01,  1.62153e+01,  1.50945e+01,  8.7061e-01,
-         -4.0965e-01,  2.7245e-01,  3.1363e-01,  3.5480e-02, -9.4888e-01,
-         -2.1492e-02, -2.1492e-01, -1.2970e-01, -8.5849e-02,  2.3618e-01,
-          2.1333e-01]]) # first row for first time step in traj7.dat
+
+        # print("X1 = ", self.item[0][0])
+        # self.item2 = self.myDataset.__getitem__(1)
+        # print("X2 = ", self.item2[0][0])
+
+        X_row1 = torch.tensor([[ 0.0000, 10.3521, 16.2153, 15.0945,  0.8706, -0.4097,  0.2724,  0.3136,
+         0.0355, -0.9489, -0.0202, -0.2138, -0.1250, -0.0863,  0.2366,  0.2093]]) # first row for first time step in traj7.dat
         self.assertAlmostEqual(float(torch.sum(self.item[0][0])), float(torch.sum(X_row1)), places=3)
         self.assertEqual(self.item[0].shape, torch.Size([40,16]))
 
@@ -85,8 +91,9 @@ class TestGetItem(TestDataset):
         Check that the function pulls the correct X data for a later time point.
         """
         self.item = self.myDataset.__getitem__(3)
+        # print(self.item[0][0])
         X_row1 = torch.tensor([[ 0.0000, 10.5120, 16.2222, 15.0403,  0.8090, -0.4704,  0.3526,  0.3778,
-        -0.0436, -0.9249,  0.5860,  0.2361, -0.1389,  0.2467, -0.1906,  0.2378]]) # fourth row for first time step in traj7.dat
+        -0.0436, -0.9249,  0.5890,  0.2334, -0.1384,  0.2456, -0.1872,  0.2404]]) # fourth row for first time step in traj7.dat
         self.assertAlmostEqual(float(torch.sum(self.item[0][0])), float(torch.sum(X_row1)), places=3)
         self.assertEqual(self.item[0].shape, torch.Size([40,16]))
 
@@ -108,6 +115,7 @@ class TestUtils(unittest.TestCase):
         self.dt = 100
         self.tf = 99900
         self.n_timesteps = int(self.tf / self.dt)
+        self.gnd_time_interval = 0.005
 
 class TestMakeGraphfromTraj(TestUtils):
 
@@ -182,9 +190,11 @@ class TestGetGroundTruthY(TestUtils):
         Check y target does the math correctly and is the right size.
         """
         full_X = buildX(self.traj_file, self.n_timesteps, self.dt, self.n_nodes, self.n_features)
-        Y_target = getGroundTruthY(self.traj_file, 4, full_X, self.dt, self.n_nodes, self.n_features)
-        Y_row1 = torch.tensor([[0.0001083, -0.0008768, 0.000861269, 0.0002895, 0.0002893, -0.00039583]])
-        self.assertAlmostEqual(float(torch.sum(Y_target[0])), float(torch.sum(Y_row1)), places=3)
+        solution = (full_X[5,0,:] - full_X[4,0,:]) / 0.5
+        solution = solution[-6:]
+        Y_target = getGroundTruthY(self.traj_file, 4, full_X, self.dt, self.n_nodes, self.n_features, self.gnd_time_interval)
+        
+        self.assertAlmostEqual(float(torch.sum(Y_target[0])), float(torch.sum(solution)), places=3)
         self.assertEqual(Y_target.shape, torch.Size([40,6]))
 
 class TestGetKNN(TestUtils):

src/gnn/utils.py
@@ -94,7 +94,7 @@ def sim2RealUnits(force_sim=None, torque_sim=None):
     return force_real_pN, torque_real_pNnm
 
 
-def doUpdate(X, Y, dt):
+def doUpdate(X, Y, dt, gnd_time_interval):
     """
     Updates the node attributes that describe each nucleotide's position (translational, rotational). Use a Euclidean update. 
 
@@ -114,6 +114,8 @@ def doUpdate(X, Y, dt):
         decoder output containing translational and rotational accelerations for each nucleotide in shape [n_nodes, n_state_vars]
     dt : int
         scalar giving the time step of the ground truth data
+    gnd_time_interval : float
+        Time represented by one time step in the ground truth data
 
     Returns: 
     --------
@@ -121,23 +123,26 @@ def doUpdate(X, Y, dt):
         node attribute matrix for the next time step in shape [n_nodes, n_features]
 
     """
+
+    delta_t = dt * gnd_time_interval
+
     # create X_next, the node attribute matrix for the next time step
     X_next = torch.zeros_like(X)
 
     # update translational velocity (vx, vy, vz)
-    X_next[:, 10:13] = X[:, 10:13] + dt * Y[:, 0:3]
+    X_next[:, 10:13] = X[:, 10:13] + delta_t * Y[:, 0:3]
 
     # update rotational velocity (Lx, Ly, Lz)
-    X_next[:, 13:] = X[:, 13:] + dt * Y[:, 3:]
+    X_next[:, 13:] = X[:, 13:] + delta_t * Y[:, 3:]
 
     # update translational position (rx, ry, rz)
-    X_next[:, 1:4] = X[:, 1:4] + dt * X_next[:, 10:13]
+    X_next[:, 1:4] = X[:, 1:4] + delta_t * X_next[:, 10:13]
 
     # update backbone base versor (bx, by, bz)
-    X_next[:, 4:7] = X[:, 4:7] + dt * X_next[:, 13:]
+    X_next[:, 4:7] = X[:, 4:7] + delta_t * X_next[:, 13:]
 
     # update normal versor (nx, ny, nz)
-    X_next[:, 7:10] = X[:, 7:10] + dt * X_next[:, 13:]
+    X_next[:, 7:10] = X[:, 7:10] + delta_t * X_next[:, 13:]
 
     return X_next
 
@@ -419,8 +424,8 @@ def buildX(traj_file, n_timesteps, dt, n_nodes, n_features)->Tensor:
 
     Returns: 
     --------
-    X : Tensor
-        node attribute matrix with values added from traj_file in shape [n_nodes, n_features]
+    full_X : Tensor
+        node attribute matrix with values added from traj_file in shape [n_timesteps+1, n_nodes, n_features]
     """
     # nucleotide dictionary
     nucleotide_dict = {
@@ -480,7 +485,8 @@ def getGroundTruthY(traj_file: str,
                     full_X : Tensor, 
                     dt: int, 
                     n_nodes: int,
-                    n_features: int)->Tensor:
+                    n_features: int,
+                    gnd_time_interval: float)->Tensor:
     """
     Computes the ground truth acceleration for a given time step t. 
 
@@ -498,12 +504,15 @@ def getGroundTruthY(traj_file: str,
         number of nodes in graph
     n_features : int
         number of features for each node
+    gnd_time_interval : float
+        Time represented by one time step in the ground truth data
     
     Returns:
     --------
     Y_target : Tensor
         a Torch tensor containing the ground truth accelerations
     """
+    delta_t = dt * gnd_time_interval
 
     # extract the data from that time step and the next one and build 2 X matrices, one for each time step
     X_t = full_X[j]
@@ -514,7 +523,7 @@ def getGroundTruthY(traj_file: str,
     v_t1 = X_t1[:, 10:]
 
     # compute the target values 
-    Y_target = (v_t1 - v_t) / dt 
+    Y_target = (v_t1 - v_t) / delta_t
 
     return Y_target
 