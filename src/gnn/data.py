fixed rollout function to include edges, adjust for normalization

src/gnn/data.py
@@ -158,7 +158,7 @@ class DatasetGraph(Dataset):
         X[:,-6:] = X[:,-6:] + noise
 
         # normalize X
-        X, self.sum, self.total_n, self.sos, _, _ = normalizeX(X, self.sum, self.total_n, self.sos)
+        X, self.sum, self.total_n, self.sos, mean, std = normalizeX(X, self.sum, self.total_n, self.sos)
         # print("X = ", X)
 
         # build up the adjacency matrix, E, for this time step
@@ -184,7 +184,7 @@ class DatasetGraph(Dataset):
         self.edge_attr = torch.from_numpy(edge_attr.T)
 
         y = getGroundTruthY(self.traj_file, j, self.full_X, self.dt, self.n_nodes, self.n_features, self.gnd_time_interval)
-        return (X, self.E, self.edge_attr, self.edge_index, y)
+        return (X, self.E, self.edge_attr, self.edge_index, y, mean, std)
     
     def __len__(self) -> int: 
         """

src/gnn/main.py
@@ -104,8 +104,6 @@ def main(args):
             }
     )
 
-    # TODO: write a function that prints these conditions neatly at the start of the run
-
     # --- variables for storing loss ---
     train_loss_list = np.zeros((epochs, n_train, n_timesteps)) # epochs, n_train files, n_timesteps
     val_loss_list = np.zeros((epochs, n_val, n_timesteps)) # epochs, n_validation files, n_timesteps
@@ -121,7 +119,7 @@ def main(args):
     val_dataloader = DataloaderGraph(val_dataset, n_timesteps, shuffle=False)
 
     # --- plot the graph ---
-    (X0, E0, _, _, _) = next(iter(train_dataloader))
+    (X0, E0, _, _, _, _, _) = next(iter(train_dataloader))
     if show_plot == True:
         plotGraph(X0,E0)
 
@@ -133,7 +131,6 @@ def main(args):
 
     # --- loss function ---
     loss_fn = nn.MSELoss() # this is used for training the model
-    # mae_loss_fn = nn.L1Loss() # this is used for comparison with other models
 
     # --- optimizer ---
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -152,7 +149,7 @@ def main(args):
         for k, batch in tqdm(enumerate(train_dataloader)):
 
             # get next dataset
-            (X, _, edge_attr, edge_index, target) = batch
+            (X, _, edge_attr, edge_index, target, _, _) = batch
 
             if architecture == "mlp":
                 y_h = model(X)
@@ -188,7 +185,7 @@ def main(args):
         for k, batch in tqdm(enumerate(val_dataloader)):
 
             # get next dataset
-            (X, _, edge_attr, edge_index, target) = batch
+            (X, _, edge_attr, edge_index, target, _, _) = batch
 
             if architecture == "mlp":
                 y_h = model(X)
@@ -250,11 +247,12 @@ def main(args):
     wandb.log({"complete loss curve": wandb.Image(complete_loss_curve_filename)})
 
     print("Final train MSE loss = ", train_loss_mean[-1])
-    train_loss_pN, _ = sim2RealUnits(math.sqrt(train_loss_mean[-1]))
-    print("Final train loss [pn] = ", train_loss_pN)
+    # train_loss_pN, _ = sim2RealUnits(math.sqrt(train_loss_mean[-1]))
+    # print("Final train loss [pn] = ", train_loss_pN)
 
     # log metrics to wandb
-    wandb.log({"final_train_MSE": train_loss_mean[-1], "final_train_loss_pn": train_loss_pN})
+    # wandb.log({"final_train_MSE": train_loss_mean[-1], "final_train_loss_pn": train_loss_pN})
+    wandb.log({"final_train_MSE": train_loss_mean[-1]})
 
     # --- save final checkpoint ---
     path = train_dir + "final_checkpoint.pt"
@@ -271,7 +269,8 @@ def main(args):
         print("---- Rollout ----")
         rollout_traj_file = train_dir + "rollout.dat"
         t0 = 100
-        model.rollout(rollout_steps, rollout_traj_file, t0, top_file, traj_file, dt, n_nodes)      
+        (X_norm, _, _, _, _, mean, std) = next(iter(train_dataloader))
+        model.rollout(k, X_norm, mean, std, rollout_steps, rollout_traj_file, t0, top_file, traj_file, dt, n_nodes)      
 
     # [optional] finish the wandb run, necessary in notebooks
     wandb.finish()

src/gnn/model.py
@@ -5,8 +5,10 @@ from torch import nn
 from torch_geometric.nn import MetaLayer
 from torch_scatter import scatter_mean
 from tqdm import tqdm
-from utils import makeGraphfromTraj, plotGraph, doUpdate, prepareEForModel
+from utils import makeGraphfromTraj, plotGraph, doUpdate, prepareEForModel, reverseNormalizeX, normalizeX
 from data import DatasetGraph, DataloaderGraph
+from torch_cluster.knn import knn_graph
+
 
 ####################
 # MLP baseline model
@@ -78,12 +80,20 @@ class MlpModel(nn.Module):
         y_h = self.mlp(X)
         return y_h
     
-    def rollout(self, rollout_steps, rollout_traj_file, t, top_file, traj_file, dt, N):
+    def rollout(self, k, X_norm, mean, std, rollout_steps, rollout_traj_file, t, top_file, traj_file, dt, N):
         """
         This function computes the trajectory for a given structure (defined by traj_file, top_file) for some time steps rollout_steps. Does not compare to ground truth - used to evaluate model's prediction capabilities. Rollout is saved to file.
 
         Parameters:
         -----------
+        k : int
+            Not actually required here.
+        X_norm : Tensor
+            Normalized X node attribute matrix.
+        mean : Tensor
+            Mean for each column in X
+        std : Tensor
+            Standard deviation for each column in X
         rollout_steps : int
             The number of time steps to generate a trajectory for.
         rollout_traj_file : str
@@ -105,15 +115,18 @@ class MlpModel(nn.Module):
        
         with torch.no_grad():
 
-            X, _ = makeGraphfromTraj(top_file, traj_file, N)
+            # X, _ = makeGraphfromTraj(top_file, traj_file, N)
+            # get normalized X from dataset class, reverse normalization
+            X_unnorm = reverseNormalizeX(X_norm, mean, std)
+            
 
-            # save X to file 
+            # save unnormalized X to file 
             with open(rollout_traj_file, "w") as f:
                 f.write("t = {0}\n".format(t))
                 f.write("b = 10 10 10\n")
                 f.write("E = 0 0 0\n")
 
-                X_np = X.numpy()
+                X_np = X_unnorm.numpy()
                 for i in range(X_np.shape[0]):
                     my_str = ""
                     for j in range(1, X_np.shape[1]):
@@ -123,18 +136,21 @@ class MlpModel(nn.Module):
                     f.write(my_str)
 
             # generate the rollout
-            for k in tqdm(range(rollout_steps)):
+            for q in tqdm(range(rollout_steps)):
                 t += dt
-                y_h = self(X) 
-                X_next = doUpdate(X, y_h, dt, self.gnd_time_interval)
+                y_h = self(X_norm) 
+                X_next = doUpdate(X_norm, y_h, dt, self.gnd_time_interval)
 
-                # save the X_next to file
+                # reverse normalization of X_next
+                X_next_unnorm = reverseNormalizeX(X_next, mean, std)
+
+                # save the unnormalized X_next to file
                 with open(rollout_traj_file, "a") as f:
                     f.write("t = {0}\n".format(t))
                     f.write("b = 84.160285949707 84.160285949707 84.160285949707\n")
                     f.write("E = 0 0 0\n")
 
-                    X_next_np = X_next.numpy()
+                    X_next_np = X_next_unnorm.numpy()
                     for i in range(X_next_np.shape[0]):
                         my_str = ""
                         for j in range(1, X_next_np.shape[1]):
@@ -143,7 +159,7 @@ class MlpModel(nn.Module):
                         my_str += "\n"
                         f.write(my_str)
 
-                X = X_next
+                X_norm = X_next
 
 
 
@@ -799,12 +815,20 @@ class GNN(nn.Module):
         # return rand_idx, preds, X_next
         return y_h, X_next
 
-    def rollout(self, rollout_steps, rollout_traj_file, t, top_file, traj_file, dt, N):
+    def rollout(self, k, X_norm, mean, std, rollout_steps, rollout_traj_file, t, top_file, traj_file, dt, N):
         """
         This function computes the trajectory for a given structure (defined by traj_file, top_file) for some time steps rollout_steps. Does not compare to ground truth - used to evaluate model's prediction capabilities. Rollout is saved to file.
 
         Parameters:
         -----------
+        k : int
+            Number of neighbors to consider
+        X_norm : Tensor
+            Normalized X node attribute matrix.
+        mean : Tensor
+            Mean for each column in X
+        std : Tensor
+            Standard deviation for each column in X
         rollout_steps : int
             The number of time steps to generate a trajectory for.
         rollout_traj_file : str
@@ -825,16 +849,18 @@ class GNN(nn.Module):
         """
        
         with torch.no_grad():
-            X, E = makeGraphfromTraj(top_file, traj_file, N)
-            edge_attr, edge_index, edge_index_coo = prepareEForModel(E)
+            _, E_backbone = makeGraphfromTraj(top_file, traj_file, N)
 
-            # save X to file 
+            # get normalized X from dataset class, reverse normalization
+            X_unnorm = reverseNormalizeX(X_norm, mean, std)
+
+            # save unnormalized X to file 
             with open(rollout_traj_file, "w") as f:
                 f.write("t = {0}\n".format(t))
                 f.write("b = 10 10 10\n")
                 f.write("E = 0 0 0\n")
 
-                X_np = X.numpy()
+                X_np = X_unnorm.numpy()
                 for i in range(X_np.shape[0]):
                     my_str = ""
                     for j in range(1, X_np.shape[1]):
@@ -843,17 +869,47 @@ class GNN(nn.Module):
                     my_str += "\n"
                     f.write(my_str)
 
-            for k in tqdm(range(rollout_steps)):
-                t += dt
-                _, X_next = self(X, edge_index, edge_attr, dt, N=N) 
+            for z in tqdm(range(rollout_steps)):
 
-                # save the X_next to file
+                # build up the adjacency matrix, E, for this time step
+                # compute E_neighbors by providing X[:,1:3] to knn_graph and asking for k nearest neighbors
+                output = knn_graph(X_norm[:,1:4], k, flow="target_to_source")
+                row = output[0,:]
+                col = output[1,:]
+                data = torch.ones_like(row)
+                coo = coo_matrix((data, (row, col)), shape=(X_norm.shape[0], X_norm.shape[0]))
+                E_knn = torch.from_numpy(coo.todense())
+                
+                # combines the backbone edges and knn edges
+                E = E_knn + E_backbone
+
+                # print("E backbone = ", E_backbone[0])
+                # print("E knn = ", E_knn[0])
+                # print("Sum of E = ", torch.sum(E, axis=1))
+                # plotGraph(X_norm, E)
+
+                # convert the output to a coo-matrix
+                edges_coo = coo_matrix(E)
+                edge_attr = np.array([edges_coo.data], dtype=np.int_)
+                edge_index = np.array([[edges_coo.row], [edges_coo.col]], dtype=np.int_)
+                edge_index = np.reshape(edge_index, (edge_index.shape[0], edge_index.shape[2]))
+
+                # convert to torch tensors
+                edge_index = torch.from_numpy(edge_index)
+                edge_attr = torch.from_numpy(edge_attr.T)
+
+                t += dt
+                _, X_next = self(X_norm, edge_index, edge_attr, dt, N=N) 
+
+                X_next_unnorm = reverseNormalizeX(X_next, mean, std)
+
+                # save the unnormalized X_next to file
                 with open(rollout_traj_file, "a") as f:
                     f.write("t = {0}\n".format(t))
                     f.write("b = 84.160285949707 84.160285949707 84.160285949707\n")
                     f.write("E = 0 0 0\n")
 
-                    X_next_np = X_next.numpy()
+                    X_next_np = X_next_unnorm.numpy()
                     for i in range(X_next_np.shape[0]):
                         my_str = ""
                         for j in range(1, X_next_np.shape[1]):
@@ -862,5 +918,5 @@ class GNN(nn.Module):
                         my_str += "\n"
                         f.write(my_str)
 
-                X = X_next
+                X_norm = X_next
 

src/gnn/test.py
@@ -1,9 +1,11 @@
 import unittest
 from data import DatasetGraph, DataloaderGraph
-from utils import makeGraphfromTraj, buildX, getGroundTruthY, prepareEForModel, plotGraph, getKNN, normalizeX
+from utils import makeGraphfromTraj, buildX, getGroundTruthY, prepareEForModel, plotGraph, getKNN, normalizeX, reverseNormalizeX
 import torch
 import time
 from tqdm import tqdm
+from model import GNN, MlpModel
+
 
 torch.manual_seed(13)
 
@@ -117,7 +119,7 @@ class TestUtils(unittest.TestCase):
         self.n_timesteps = int(self.tf / self.dt)
         self.gnd_time_interval = 0.005
 
-class TestNormalization(TestUtils):
+class TestNormalization(TestDataset):
     
     def test_normalization1(self):
         """
@@ -136,6 +138,8 @@ class TestNormalization(TestUtils):
         sos_input = torch.zeros(1,3)
 
         X_final, sum, total_n, sos, mean, std = normalizeX(X, sum_input, total_n_input, sos_input)
+
+        X_reversed = reverseNormalizeX(X_final, mean, std)
         
         self.assertAlmostEqual(float(torch.sum(X_[0])), float(torch.sum(X_final[0])), places=2)
         self.assertAlmostEqual(float(torch.sum(sum_)), float(torch.sum(sum)), places=1)
@@ -143,6 +147,23 @@ class TestNormalization(TestUtils):
         self.assertAlmostEqual(float(torch.sum(sos_)), float(torch.sum(sos)), places=2)
         self.assertAlmostEqual(float(torch.sum(mean_)), float(torch.sum(mean)), places=2)
         self.assertAlmostEqual(float(torch.sum(std_)), float(torch.sum(std)), places=2)
+        self.assertAlmostEqual(float(torch.sum(X)), float(torch.sum(X_reversed)), places=2)
+
+    def test_normalization2(self):
+        """
+        Check the variance of the normalized data.
+        """
+
+        for i in range(5):
+            self.item = self.myDataset.__getitem__(i)
+            X_norm = self.item[0]
+            mean = self.item[-2]
+            std = self.item[-1]
+            # print("\ni = ", i)
+            # print("normalized X = ", X_norm)
+            # print("mean X = ", mean)
+            # print("std X = ", std)
+        
 
 class TestMakeGraphfromTraj(TestUtils):
 
@@ -323,6 +344,82 @@ class TestIterNextDataloader(TestDataloader):
     #     total = t1-t0
     #     print("Total time = ", total) # currently takes 44 sec - now down to 36 sec -- now down to 1.5 sec!!! 
 
+class TestRollout(unittest.TestCase):
+    def test_rollout1(self):
+        n_nodes = 40
+        n_edges = 20
+        n_features = 16
+        n_latent = 256
+        Y_features = 6
+        gnd_time_interval = 0.005
+        dt = 100
+        tf = 99900
+        n_timesteps = int(tf / dt)
+        noise_std = 0.0003
+        rollout_steps = 10
+        k = 3
+        PATH = "/home/emma/Documents/research/gnn-dna/dsdna-dataset/training/Ngnn_256_knn_3_noise_0003/checkpoint_95.pt"
+        train_dir="/home/emma/Documents/research/gnn-dna/dsdna-dataset/training/"
+        top_file = "/home/emma/repos/gnn-dna-sim/src/dataset-generation/dsDNA/top.top"
+        traj_file = "/home/emma/Documents/research/gnn-dna/dsdna-dataset/training/trajectory_sim_traj10.dat"
+
+        checkpoint = torch.load(PATH)
+        model = GNN(n_nodes, n_edges, n_features, n_latent, Y_features, gnd_time_interval) # KEEP 
+        model.load_state_dict(checkpoint['model_state_dict'])
+        model.eval()
+
+        train_dataset = DatasetGraph(train_dir, n_nodes, n_features, dt, n_timesteps, k, gnd_time_interval, noise_std)
+        train_dataloader = DataloaderGraph(train_dataset, n_timesteps, shuffle=True)
+
+        print("\n---- Rollout ----")
+        rollout_traj_file = train_dir + "rollout.dat"
+        t0 = 100
+
+        
+        myDataset = DatasetGraph(train_dir, n_nodes, n_features, dt, n_timesteps, k, gnd_time_interval, noise_std)
+        traj_file = myDataset.traj_list[0]
+        item = myDataset.__getitem__(0)
+        X_norm = item[0]
+
+        model.rollout(k, X_norm, item[-2], item[-1], rollout_steps, rollout_traj_file, t0, top_file, traj_file, dt, n_nodes)  
+
+def test_rollout2(self):
+        n_nodes = 40
+        n_edges = 20
+        n_features = 16
+        n_latent = 128
+        Y_features = 6
+        gnd_time_interval = 0.005
+        dt = 100
+        tf = 99900
+        n_timesteps = int(tf / dt)
+        noise_std = 0.0003
+        rollout_steps = 10
+        k = 3
+        PATH = "/home/emma/Documents/research/gnn-dna/dsdna-dataset/training/Ngnn_128_knn_3_noise_0003/checkpoint_95.pt"
+        train_dir="/home/emma/Documents/research/gnn-dna/dsdna-dataset/training/"
+        top_file = "/home/emma/repos/gnn-dna-sim/src/dataset-generation/dsDNA/top.top"
+        traj_file = "/home/emma/Documents/research/gnn-dna/dsdna-dataset/training/trajectory_sim_traj10.dat"
+
+        checkpoint = torch.load(PATH)
+        model = GNN(n_nodes, n_edges, n_features, n_latent, Y_features, gnd_time_interval) # KEEP 
+        model.load_state_dict(checkpoint['model_state_dict'])
+        model.eval()
+
+        train_dataset = DatasetGraph(train_dir, n_nodes, n_features, dt, n_timesteps, k, gnd_time_interval, noise_std)
+        train_dataloader = DataloaderGraph(train_dataset, n_timesteps, shuffle=True)
+
+        print("\n---- Rollout ----")
+        rollout_traj_file = train_dir + "rollout.dat"
+        t0 = 100
+
+        
+        myDataset = DatasetGraph(train_dir, n_nodes, n_features, dt, n_timesteps, k, gnd_time_interval, noise_std)
+        traj_file = myDataset.traj_list[0]
+        item = myDataset.__getitem__(0)
+        X_norm = item[0]
+
+        model.rollout(k, X_norm, item[-2], item[-1], rollout_steps, rollout_traj_file, t0, top_file, traj_file, dt, n_nodes)  
 
 
 if __name__ == '__main__':

src/gnn/utils.py
@@ -6,61 +6,68 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import Tuple
 
+def reverseNormalizeX(X_norm : Tensor, mean : Tensor, std : Tensor):
+    """
+    Reverses the normalization of node attribute matrix X.
+    """
+    X = (X_norm * std) + mean
+    return X
+
 def normalizeX(X : Tensor, sum : Tensor, total_n : int, sos : Tensor):
-        """
-        Normalizes the node attribute matrix X and updates running statistics.
+    """
+    Normalizes the node attribute matrix X and updates running statistics.
 
-        Parameters:
-        -----------
-        X : Tensor
-            Node attribute matrix for one time step of size [n_nodes, n_features]
-        sum : Tensor
-            Sum of all nodes for all time steps for each column of size [1, n_features]
-        total_n : int
-            Total number of nodes considered for all time steps so far
-        sos : Tensor
-            Sum of squares (x_i - mean)**2 for all time steps for all nodes for each column of size [1, n_features]
+    Parameters:
+    -----------
+    X : Tensor
+        Node attribute matrix for one time step of size [n_nodes, n_features]
+    sum : Tensor
+        Sum of all nodes for all time steps for each column of size [1, n_features]
+    total_n : int
+        Total number of nodes considered for all time steps so far
+    sos : Tensor
+        Sum of squares (x_i - mean)**2 for all time steps for all nodes for each column of size [1, n_features]
 
-        Returns:
-        --------
-        X : Tensor
-            Normalized version of X
-        sum : Tensor
-            Updated version of sum
-        total_n : int
-            Updated version of total_n
-        sos : Tensor
-            Updated version of sos
-        mean : Tensor
-            Mean computed for all nodes so far for each column of size [1, n_features]
-        std : Tensor
-            Standard deviation for all nodes so far for each column of size [1, n_features]
-        """
+    Returns:
+    --------
+    X : Tensor
+        Normalized version of X
+    sum : Tensor
+        Updated version of sum
+    total_n : int
+        Updated version of total_n
+    sos : Tensor
+        Updated version of sos
+    mean : Tensor
+        Mean computed for all nodes so far for each column of size [1, n_features]
+    std : Tensor
+        Standard deviation for all nodes so far for each column of size [1, n_features]
+    """
 
-        # update sum of entries in every column, use all nodes
-        sum += torch.sum(X, 0) # sum over all the columns
+    # update sum of entries in every column, use all nodes
+    sum += torch.sum(X, 0) # sum over all the columns
 
-        # update total number of nodes, n -- count each node individually
-        total_n += X.shape[0]
+    # update total number of nodes, n -- count each node individually
+    total_n += X.shape[0]
 
-        # mean = sum / n  
-        mean = sum / total_n
+    # mean = sum / n  
+    mean = sum / total_n
 
-        # update sum of squares in every column, use all nodes
-        sos += torch.sum(((X-mean)**2),0)
+    # update sum of squares in every column, use all nodes
+    sos += torch.sum(((X-mean)**2),0)
 
-        # recompute mean and variance
-        # sigma = sum (x_i - mean_x)^2 / (n-1)
-        sigma = sos / (total_n - 1)
+    # recompute mean and variance
+    # sigma = sum (x_i - mean_x)^2 / (n-1)
+    sigma = sos / (total_n - 1)
 
-        # std = sqrt(sigma)
-        std = torch.sqrt(sigma)
+    # std = sqrt(sigma)
+    std = torch.sqrt(sigma)
 
-        # apply normalization
-        # for each entry in each column, x_i - mean / std
-        X = (X - mean) / std
+    # apply normalization
+    # for each entry in each column, x_i - mean / std
+    X = (X - mean) / std
 
-        return X, sum, total_n, sos, mean, std
+    return X, sum, total_n, sos, mean, std
 
 def getKNN(X : Tensor, k : int = 3):
     """